d5a3912548f05bcfcb292d46a62828cbe6cf7602
GUILHERME ASAI OLIVEIRA <007685076@itaud.des.ihf>
2020-06-25 18:01:49 -0300

    aula web com springboot

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/demo")
+	public String sayHello() {
+		return "Meu primeiro exemplo spring boot!";
+	}
+	
+}
---
+++ demo/src/main/java/com/example/demo/produto/Produto.java
@@ -0,0 +1,15 @@
+package com.example.demo.produto;
+
+public class Produto {
+	private int id;
+
+	int getId() {
+		return id;
+	}
+
+	void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
